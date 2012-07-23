3873721f4b1ad43d841483fdca7c752f85fd969d
Alin Dreghiciu <adreghiciu@gmail.com>
2012-07-23 15:26:50 +0300

    [NXCM-4522] Extract TemplateEngine iface
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- template-engine/src/main/java/org/sonatype/sisu/goodies/templateengine/TemplateEngine.java
+++ template-engine/src/main/java/org/sonatype/sisu/goodies/templateengine/TemplateEngine.java
@@ -7,115 +7,26 @@
  */
 package org.sonatype.sisu.goodies.templateengine;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
 import java.net.URL;
 import java.util.Map;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
 
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.Nullable;
-import org.sonatype.sisu.goodies.common.ComponentSupport;
-import org.sonatype.sisu.goodies.common.io.Closer;
-import org.sonatype.sisu.velocity.Velocity;
-import com.google.common.base.Throwables;
-import com.google.common.collect.Maps;
 
 /**
  * Provides template rendering functionality.
  *
  * @since 1.4
  */
-@Named
-@Singleton
-public class TemplateEngine
-    extends ComponentSupport
+public interface TemplateEngine
 {
 
-    private final Velocity velocity;
+    String render( Object owner, URL template, @Nullable Map<String, Object> params );
 
-    @Inject
-    public TemplateEngine( final Velocity velocity )
-    {
-        this.velocity = checkNotNull( velocity );
-    }
+    String render( Object owner, URL template, TemplateParameters params );
 
-    // FIXME: Sort out how we can use a better style-sheet for these templates so they can use normal-ish markup and not ugly verbose stuff we have to do now due to extjs style being messed up.
+    String render( Object owner, @NonNls String template, @Nullable Map<String, Object> params );
 
-    // FIXME: Sort out how to use Velocity template mechanism properly in NX
-
-    public String render( final Object owner, final @NonNls URL template, @Nullable Map<String, Object> params )
-    {
-        checkNotNull( template );
-        // params can be null
-
-        log.trace( "Rendering template: {} w/params: {}", template, params );
-
-        if ( params == null )
-        {
-            params = Maps.newHashMap();
-        }
-
-        Reader input = null;
-        try
-        {
-            input = new InputStreamReader( template.openStream() );
-
-            VelocityEngine engine = velocity.getEngine();
-            params.put( "owner", owner ); //NON-NLS
-
-            StringWriter buff = new StringWriter();
-            engine.evaluate( new VelocityContext( params ), buff, template.getFile(), input ); //NON-NLS
-
-            String result = buff.toString();
-            log.trace( "Result: {}", result );
-
-            return result;
-        }
-        catch ( Exception e )
-        {
-            throw Throwables.propagate( e );
-        }
-        finally
-        {
-            Closer.close( input );
-        }
-    }
-
-    public String render( final Object owner, final URL template, TemplateParameters params )
-    {
-        checkNotNull( params );
-        return render( owner, template, params.get() );
-    }
-
-    public String render( final Object owner, final @NonNls String template, @Nullable Map<String, Object> params )
-    {
-        checkNotNull( template );
-        // params can be null
-
-        log.trace( "Rendering template: {} w/params: {}", template, params );
-
-        URL resource = owner.getClass().getResource( template );
-        if ( resource == null )
-        {
-            log.warn( "Missing resource for template: {}; for owner: {}", template, owner.getClass().getName() );
-            return null;
-        }
-
-        return render( owner, resource, params );
-    }
-
-    public String render( final Object owner, final @NonNls String template, TemplateParameters params )
-    {
-        checkNotNull( params );
-        return render( owner, template, params.get() );
-    }
+    String render( Object owner, @NonNls String template, TemplateParameters params );
 
 }
