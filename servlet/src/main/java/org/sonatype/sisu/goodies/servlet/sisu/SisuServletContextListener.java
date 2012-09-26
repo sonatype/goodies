9c62d59dd25c80a84537d4db651b20f80ae987a1
Jason Dillon <jason@planet57.com>
2012-09-26 12:02:04 -0700

    Add filter support to sort out getting into http-land which is where most bits will want to be

--- servlet/src/main/java/org/sonatype/sisu/goodies/servlet/sisu/SisuServletContextListener.java
+++ servlet/src/main/java/org/sonatype/sisu/goodies/servlet/sisu/SisuServletContextListener.java
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package org.sonatype.sisu.goodies.servlet;
+package org.sonatype.sisu.goodies.servlet.sisu;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Guice;
