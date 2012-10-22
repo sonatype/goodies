ef445600ec884166a112b9a624aab3a64430a7eb
Jason Dillon <jason@planet57.com>
2012-10-22 11:51:09 -0700

    Add basic sanity test for mem pref factory

---
+++ prefs/src/test/java/org/sonatype/sisu/goodies/prefs/memory/MemoryPreferencesFactoryTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2007-2012 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.sisu.goodies.prefs.memory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonatype.sisu.litmus.testsupport.TestSupport;
+
+import java.util.prefs.Preferences;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for {@link MemoryPreferencesFactory}.
+ */
+public class MemoryPreferencesFactoryTest
+    extends TestSupport
+{
+    private MemoryPreferencesFactory factory;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new MemoryPreferencesFactory();
+    }
+
+    @Test
+    public void getSystemRoot() {
+        Preferences pref = factory.systemRoot();
+        assertThat(pref, notNullValue());
+        assertThat(pref.name(), is(MemoryPreferences.ROOT_NAME));
+    }
+
+    @Test
+    public void getUserRoot() {
+        Preferences pref = factory.userRoot();
+        assertThat(pref, notNullValue());
+        assertThat(pref.name(), is(MemoryPreferences.ROOT_NAME));
+    }
+}
