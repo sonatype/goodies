edab0480a04c947c19bc5ebb4b2d4c0025ffb829
Alin Dreghiciu <adreghiciu@gmail.com>
2013-10-04 17:07:21 +0300

    [NEXUS-5973] Add utility methods to retrieve and verify a PGP signature (to goodies-crypto)
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- crypto/src/main/java/org/sonatype/sisu/goodies/crypto/CryptoHelper.java
+++ crypto/src/main/java/org/sonatype/sisu/goodies/crypto/CryptoHelper.java
@@ -13,6 +13,8 @@
 
 package org.sonatype.sisu.goodies.crypto;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -20,6 +22,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Signature;
+import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
@@ -27,6 +30,11 @@ import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
+
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.PGPSignatureList;
 
 /**
  * Provides access to cryptology components.
@@ -59,5 +67,33 @@ public interface CryptoHelper
   TrustManagerFactory createTrustManagerFactory(String algorithm) throws NoSuchAlgorithmException;
 
   MessageDigest createDigest(String algorithm) throws NoSuchAlgorithmException;
+
+  /**
+   * Reads PGP signatures from an input stream.
+   *
+   * @param inputStream to read from
+   * @return PGP signatures (never null)
+   * @throws IOException  If a problem occurred while reading the provided stream
+   * @throws PGPException If there are no PGP objects or signatures in provided stream
+   * @since 1.8
+   */
+  PGPSignatureList readPGPSignatures(InputStream inputStream) throws IOException, PGPException;
+
+  /**
+   * Verify a PGP signed content.
+   *
+   * @param inputStream to verify (cannot be null)
+   * @param signature   PGP signature (cannot be null)
+   * @param publicKey   public key used to sign (cannot be null)
+   * @return true if the signature verifies, false otherwise
+   * @throws IOException        If a problem occurred while reading the provided stream
+   * @throws PGPException       If PGP verifier could not be built
+   * @throws SignatureException If a problem occurred during verification process
+   * @since 1.8
+   */
+  boolean verifyPGPSignature(InputStream inputStream,
+                             PGPSignature signature,
+                             PGPPublicKey publicKey)
+      throws IOException, PGPException, SignatureException;
 }
 
