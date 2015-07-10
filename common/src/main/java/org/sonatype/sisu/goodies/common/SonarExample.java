1b33ce7e16add4bce3b60deeb8f650ad1d14cd82
Jason Dillon <jason@planet57.com>
2015-07-09 17:05:15 -0700

    Add silly test for sonar

---
+++ common/src/main/java/org/sonatype/sisu/goodies/common/SonarExample.java
@@ -0,0 +1,12 @@
+package org.sonatype.sisu.goodies.common;
+
+/**
+ * Created by jason on 7/9/15.
+ */
+public class SonarExample
+{
+  public SonarExample() {
+    int foo = 1;
+    foo = foo;
+  }
+}
