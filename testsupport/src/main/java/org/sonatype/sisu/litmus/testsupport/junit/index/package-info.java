35eae9e2499dd59d426a5dae7a9d5e97af834033
Jason Dillon <jason@planet57.com>
2015-09-09 15:02:00 -0700

    Update headers

--- testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/junit/index/package-info.java
+++ testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/junit/index/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007-2014 Sonatype, Inc. All rights reserved.
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,7 +10,6 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-
 // additional schema declarations needed in Java7+ to match original behaviour
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://sonatype.com/xsd/litmus-testsupport/index/1.0", xmlns = { //
 @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://sonatype.com/xsd/litmus-testsupport/index/1.0", prefix = "index") })
