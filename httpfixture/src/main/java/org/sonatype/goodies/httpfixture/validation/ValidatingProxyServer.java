ec9179967273411d5c763dc90ad9f51eb588494e
Stuart McCulloch <mcculls@gmail.com>
2018-05-01 13:57:43 +0100

    Adapt validating proxy server to work with Guava < and >= 20.0 (#58)

--- httpfixture/src/main/java/org/sonatype/goodies/httpfixture/validation/ValidatingProxyServer.java
+++ httpfixture/src/main/java/org/sonatype/goodies/httpfixture/validation/ValidatingProxyServer.java
@@ -12,20 +12,26 @@
  */
 package org.sonatype.goodies.httpfixture.validation;
 
+import java.net.InetSocketAddress;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.common.net.HostAndPort;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import org.littleshoot.proxy.DefaultHostResolver;
+import org.littleshoot.proxy.HostResolver;
 import org.littleshoot.proxy.HttpFilters;
 import org.littleshoot.proxy.HttpFiltersAdapter;
 import org.littleshoot.proxy.HttpFiltersSourceAdapter;
 import org.littleshoot.proxy.HttpProxyServer;
 import org.littleshoot.proxy.HttpProxyServerBootstrap;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
@@ -41,11 +47,13 @@ import static com.google.common.base.Preconditions.checkState;
  */
 public class ValidatingProxyServer
 {
+  private static final Logger log = LoggerFactory.getLogger(ValidatingProxyServer.class);
+
   private HttpProxyServer server;
 
   private AtomicInteger successCount = new AtomicInteger();
 
-  private int port = 0;
+  private int serverPort = 0;
 
   private final HttpValidator[] validators;
 
@@ -86,7 +94,7 @@ public class ValidatingProxyServer
 
   public ValidatingProxyServer withPort(int port) {
     checkArgument(port > 0, "Must have port greater than zero.");
-    this.port = port;
+    this.serverPort = port;
 
     return this;
   }
@@ -99,14 +107,19 @@ public class ValidatingProxyServer
    * Generate an {@link HttpProxyServerBootstrap} which validates requests using the given {@link HttpValidator}
    * object(s).
    */
-  private HttpProxyServerBootstrap createWithValidation(final HttpValidator... validators) {
-    return DefaultHttpProxyServer.bootstrap().withPort(port).withAllowLocalOnly(true).withAuthenticateSslClients(false)
+  private HttpProxyServerBootstrap createWithValidation(final HttpValidator... validators) { // NOSONAR
+    return DefaultHttpProxyServer.bootstrap()
+        .withPort(serverPort)
+        .withAllowLocalOnly(true)
+        .withAuthenticateSslClients(false)
         .withFiltersSource(new HttpFiltersSourceAdapter()
         {
           @Override
-          public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
+          public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) { // NOSONAR
             return new HttpFiltersAdapter(originalRequest)
             {
+              private final HostResolver hostResolver = new DefaultHostResolver();
+
               @Override
               public HttpResponse clientToProxyRequest(HttpObject httpObject) {
                 HttpServletRequest req = new NettyHttpRequestWrapper(originalRequest);
@@ -115,6 +128,32 @@ public class ValidatingProxyServer
                 }
                 successCount.incrementAndGet();
                 return null;
+              }
+
+              /**
+               * Workaround https://github.com/adamfisk/LittleProxy/issues/398 by overriding default parsing.
+               */
+              @Override
+              public InetSocketAddress proxyToServerResolutionStarted(final String resolvingServerHostAndPort) {
+                try {
+                  HostAndPort hostAndPort = HostAndPort.fromString(resolvingServerHostAndPort);
+                  String host = getHost(hostAndPort);
+                  int port = hostAndPort.getPortOrDefault(80);
+                  return hostResolver.resolve(host, port);
+                }
+                catch (Exception e) {
+                  log.warn("Problem resolving {}", resolvingServerHostAndPort, e);
+                  return null; // we can't resolve this host, return null to fall back to normal DNS resolution
+                }
+              }
+
+              private String getHost(final HostAndPort hostAndPort) {
+                try {
+                  return hostAndPort.getHost();
+                }
+                catch (LinkageError e) { // NOSONAR
+                  return hostAndPort.getHostText(); // fall back to deprecated method on old versions of Guava
+                }
               }
             };
           }
